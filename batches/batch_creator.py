21c3908ad2fbfc0892854ce3637bf22f620c523a
jareds-laf <sofairj@lafayette.edu>
2022-07-05 12:48:33 -0400

    Batch updates
    Updated batch_creator.py to (hopefully) be fully compatible with the HPC:
    - Updated all hpc file paths
    
    - Expanded analysis options functionality to work with Gaia and RUWE tags (still need to add RVs)
    
    - Changed age tag to be more robust (more details later)
    
    - Wrote a test .bash file, which currently shows the Windows path names for testing purposes.
    
    Changed the location of pm and targets_abr (I had previously placed them in the incorrect folder).
    
    Manually added a general approximation for the age of our targets that fits our BHAC models. Done so that the age specification could be done to individual stars rather than to an entire batch.

--- batches/batch_creator.py
+++ batches/batch_creator.py
@@ -9,36 +9,39 @@ from astropy.table import Table
 import numpy as np
 
 
-# csv_path_drive = os.path.expanduser(r'G:\Shared drives\DouglasGroup\Jared Sofair 2022\CSV Files')
-# csv_path_github = os.path.expanduser(r'C:\Users\Jared\Documents\GitHub\data-parser\CSV Files')
-# contrast_path = os.path.expanduser(r'G:\Shared drives\DouglasGroup\Jared Sofair 2022\MOLUSC\Data Parser Tables')
-csv_path_hpc = os.path.expanduser(r'../code')
+csv_path_drive = os.path.expanduser(r'G:\Shared drives\DouglasGroup\Jared Sofair 2022\CSV Files')
+csv_path_github = os.path.expanduser(r'C:\Users\Jared\Documents\GitHub\data-parser\CSV Files')
+contrast_path = os.path.expanduser(r'G:\Shared drives\DouglasGroup\Jared Sofair 2022\MOLUSC\Data Parser Tables')
+csv_path_hpc = os.path.expanduser(r'../csvs')
 contrast_path_hpc = os.path.expanduser(r'../contrasts')
 
-# anaconda_path = os.path.expanduser(r'C:\Users\Jared\anaconda3')
-anaconda_path_hpc = os.path.expanduser(r'C:\Users\Jared\anaconda3')
+anaconda_path = os.path.expanduser(r'C:\Users\Jared\anaconda3')
 
-# repo_path = os.path.expanduser(r'C:\Users\Jared\Documents\GitHub\MOLUSC')
-repo_path_hpc = os.path.expanduser(r'../MOLUSC')
+repo_path = os.path.expanduser(r'C:\Users\Jared\Documents\GitHub\MOLUSC')
 gui_path = os.path.join(repo_path, r'code/BinaryStarGUI.py')
 batch_path = os.path.join(repo_path, r'batches')
+repo_path_hpc = os.path.expanduser(r'../MOLUSC')
+gui_path_hpc = os.path.join(repo_path_hpc, r'code/BinaryStarGUI.py')
+batch_path_hpc = os.path.join(repo_path_hpc, r'batches')
 
-# output_path_drive = os.path.expanduser(r'G:\Shared drives\DouglasGroup\Jared Sofair 2022\MOLUSC\MOLUSC Outputs\Tables')
+output_path_drive = os.path.expanduser(r'G:\Shared drives\DouglasGroup\Jared Sofair 2022\MOLUSC\MOLUSC Outputs\Tables')
 output_path_hpc = os.path.expanduser(r'../outputs/tables')
 
-pm = Table.read(os.path.join(csv_path_drive, r'Praesepe_Merged.csv'))
-targets = Table.read(os.path.join(csv_path_github, r'targets_abr.csv'))
+pm = Table.read(os.path.join(csv_path_hpc, r'Praesepe_Merged.csv'))
+targets = Table.read(os.path.join(csv_path_hpc, r'targets_abr.csv'))
+
 
 # Format for running star through MOLUSC in command line
 # python [GUI code path] [input data type(s)] [contrast/RV path] [input args] [output path] [other input args]
 # Example:
 # python "C:/Users/Jared/Documents/GitHub/MOLUSC/code/BinaryStarGUI.py" -v -a --ao "G:/Shared drives/DouglasGroup/Jared Sofair 2022/MOLUSC/Data Parser Tables/JS355.txt" --filter K --age 0.8000 "G:/Shared drives/DouglasGroup/Jared Sofair 2022/MOLUSC/MOLUSC Outputs/Tables/JS355" 08h40m22.16s -- +18d07m24.8s 3 0.599
 
-def run_batch_stars(stars=["JS355"], analysis_options="ao", write_all=True, extra_output=True, filt=None, age=0.015, companions=3, opsys="win"):
+def run_batch_stars(stars=["JS355"], analysis_options=["ao"], write_all=True, extra_output=True, filt=None, companions=3, opsys="win"):
     if opsys=="win":
-        # Create .bat file, write line necessary to run the script for Windows
+        #%% Create .bat file, write line necessary to run the script for Windows
         with open(os.path.join(batch_path, r"batch_runner.bat"), 'w') as f:
-            
+            # pm = Table.read(os.path.join(csv_path_github, r'Praesepe_Merged.csv'))
+            # targets = Table.read(os.path.join(csv_path_github, r'targets_abr.csv'))
             # Python seems to have trouble with the back slashes, so I separated this into 3 lines
             f.write(f'call {anaconda_path}')
             f.write(r'\Scripts\activate.bat ')
@@ -58,17 +61,22 @@ def run_batch_stars(stars=["JS355"], analysis_options="ao", write_all=True, extr
                 
                 # Analysis options and output
                 if "ao" in analysis_options: # HRI
-                    f.write(f'--{analysis_options} "{os.path.join(contrast_path, star.replace(" ", "_"))}.txt" --filter {filt} ')
+                    f.write(f'--ao "{os.path.join(contrast_path, star.replace(" ", "_"))}.txt" --filter {filt} ')
                 # if "rv" in analysis_options: # RV
-                # if "ruwe" in analysis_options: # RUWE
-                # if "gaia" in analysis_options: # Gaia
+                #   f.write(r'--rv "{os.path.join(rv_path, star.replace(" ", "_"))}.txt" --resolution 50000')
+                if "ruwe" in analysis_options: # RUWE
+                    f.write('--gaia ')
+                if "gaia" in analysis_options: # Gaia
+                    f.write('--ruwe ')
                 
                 # Star info (age, output path, ra, dec, # companions, mass)
                 # Get ra, dec, and mass
                 ra = targets["ra"][np.where(targets["name"] == star)[0][0]]
                 dec = targets["de"][np.where(targets["name"] == star)[0][0]]
                 mass = np.round(targets["M/Ms"][np.where(targets["name"] == star)[0][0]], 3)
-                f.write(f'--age {age} "{os.path.join(output_path, star.replace(" ", "_"))}" {ra} -- +{dec} {companions} {mass}\n\n')
+                age = targets["age"][np.where(targets["name"] == star)[0][0]]
+                print(age)
+                f.write(f'--age {age} "{os.path.join(output_path_drive, star.replace(" ", "_"))}" {ra} -- +{dec} {companions} {mass}\n\n')
             f.write('pause')
     
     # Choices:
@@ -97,13 +105,13 @@ def run_batch_stars(stars=["JS355"], analysis_options="ao", write_all=True, extr
             # Extra output
     
     else:
-        # Create .bat file, write line necessary to run the script for Windows
-        with open(os.path.join(batch_path, r"batch_runner.bash"), 'w') as f:
+        #%% Create .bat file, write line necessary to run the script for Linux
+        with open(os.path.join(batch_path_hpc, r"batch_runner.bash"), 'w') as f:
                        
             # Write the command for each star
             for star in stars:
                     
-                f.write(f'# {star}\npython "{gui_path}" ') # Label for readability :)
+                f.write(f'# {star}\npython "{gui_path_hpc}" ') # Label for readability :)
                 
                 # Write all
                 if write_all == True:
@@ -112,20 +120,23 @@ def run_batch_stars(stars=["JS355"], analysis_options="ao", write_all=True, extr
                 if extra_output == True:
                     f.write('-a ')
                 
-                # Analysis options and output
                 if "ao" in analysis_options: # HRI
-                    f.write(f'--{analysis_options} "{os.path.join(contrast_path, star.replace(" ", "_"))}.txt" --filter {filt} ')
+                    f.write(f'--ao "{os.path.join(contrast_path_hpc, star.replace(" ", "_"))}.txt" --filter {filt} ')
                 # if "rv" in analysis_options: # RV
-                # if "ruwe" in analysis_options: # RUWE
-                # if "gaia" in analysis_options: # Gaia
+                #   f.write(r'--rv "{os.path.join(rv_path, star.replace(" ", "_"))}.txt" --resolution 50000')
+                if "ruwe" in analysis_options: # RUWE
+                    f.write('--gaia ')
+                if "gaia" in analysis_options: # Gaia
+                    f.write('--ruwe ')
                 
                 # Star info (age, output path, ra, dec, # companions, mass)
                 # Get ra, dec, and mass
                 ra = targets["ra"][np.where(targets["name"] == star)[0][0]]
                 dec = targets["de"][np.where(targets["name"] == star)[0][0]]
                 mass = np.round(targets["M/Ms"][np.where(targets["name"] == star)[0][0]], 3)
-                f.write(f'--age {age} "{os.path.join(output_path, star.replace(" ", "_"))}" {ra} -- +{dec} {companions} {mass}\n\n')
+                age = targets["age"][np.where(targets["name"] == star)[0][0]]
+                f.write(f'--age {age} "{os.path.join(output_path_hpc, star.replace(" ", "_"))}" {ra} -- +{dec} {companions} {mass}\n\n')
         
 if __name__ == "__main__": # hehe
     all_targets = targets["name"]
-    run_batch_stars(all_targets, age=0.8000, filt="K")
+    run_batch_stars(all_targets, analysis_options=["ao"], filt="K", opsys='win')
