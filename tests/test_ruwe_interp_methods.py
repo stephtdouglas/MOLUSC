656f12b5585756ffb0994a0ef6154645e86833cd
Stephanie Douglas <stephanietdouglas@gmail.com>
2024-06-07 11:41:05 -0400

    compare original interp2d to new RegularGridInterp

---
+++ tests/test_ruwe_interp_methods.py
@@ -0,0 +1,100 @@
+import pathlib, os
+import warnings
+
+import h5py
+import pytest
+import numpy as np
+import astropy.io.ascii as at
+from astropy.table import Table
+import scipy.stats as stats
+import scipy
+
+import molusc
+repo_path = pathlib.Path(molusc.__file__).resolve().parent.parent
+from molusc.ruwe import RUWE
+from molusc.companions import Companions
+from molusc.utils import set_null_limits
+
+warnings.simplefilter('error', category=RuntimeWarning)
+
+limits = set_null_limits()
+pd_mu = 5.03
+pd_sig = 2.28
+q_exp = 0.0
+
+# Use the demo star from the github page (HD 120411/HIP 67522)
+# This is also the star shown in Figure 
+star_ra = '13h50m06.28s '
+star_dec = '-40d50m08.9s'
+star_age = 0.015
+star_mass = 1.3
+plx = 8.017 #mas
+star_ruwe = 0.91 #Rizzuto+ 2020
+
+dist_pc = 1000/plx 
+dist_au = dist_pc * 206265
+
+# Compute the limits of the RUWE grid
+log_sep_lims = np.array([1.275,3.325])
+sep_lims = 10**log_sep_lims # mas
+sep_lims_rad = np.radians(sep_lims/3.6e6)
+# Small angle approximation means that we can multiply dist * radians
+sep_lims_au = sep_lims_rad * dist_au
+
+ref_file = os.path.join(repo_path,"reference_data/RuweTableGP.txt")
+ref = at.read(ref_file)
+nref = len(ref)
+
+# Generate a set of companions for testing
+comps = Companions(100, limits, star_mass, pd_mu, pd_sig, q_exp)
+comps.generate()
+
+# This is Mackenna's original code, which uses interp2d
+# Read in the reference table
+file_name = ref_file
+t = Table.read(file_name, format='ascii', delimiter=' ')
+ruwe_dist = t
+
+# f. Get predicted RUWE
+#    convert from mas to AU
+star_distance = 1 / (plx) * 2.063e+8 # AU
+ruwe_dist['Sep(AU)'] = [star_distance * np.tan(np.radians(x/(3.6e6))) for x in np.power(10, ruwe_dist['log(sep)']) ]
+
+# Adding: modify the reference file as well
+ref_inputs = np.array([[float(row["log(sep)"]),float(row["DeltaG"])] for row in ref])
+ref_inputs_lin = np.copy(ref_inputs)
+ref_inputs_lin[:,0] = 10**ref_inputs[:,0]
+ref_inputs_star = np.copy(ref_inputs)
+ref_inputs_star[:,0] = dist_au * np.radians(ref_inputs_lin[:,0]/3.6e6)
+
+#  2D interpolation functions for ruwe and sigma_ruwe
+x_edges = np.unique(np.array(ruwe_dist['Sep(AU)']))
+y_edges = np.unique(np.array(ruwe_dist['DeltaG']))
+z = np.reshape(np.array(ruwe_dist['log(RUWE)']), [len(y_edges), len(x_edges)])
+z_sigma = np.reshape(np.array(ruwe_dist['sigma_log(RUWE)']), [len(y_edges), len(x_edges)])
+
+f_ruwe2d = scipy.interpolate.interp2d(x_edges, y_edges, z)
+f_sigma2d = scipy.interpolate.interp2d(x_edges, y_edges, z_sigma)
+
+pred_log_ruwe2d = np.concatenate([f_ruwe2d(ref_inputs_star[i][0],ref_inputs_star[i][1]) for i in range(nref)])
+predicted_ruwe2d = 10**pred_log_ruwe2d
+pred_sigma2d = np.concatenate([f_sigma2d(ref_inputs_star[i][0],ref_inputs_star[i][1]) for i in range(nref)])
+
+
+# Let's try something different. Use the exact same input arrays 
+# but interpolate with the RGI instead
+f_ruwe = scipy.interpolate.RegularGridInterpolator(points=(x_edges, y_edges), values=z.T,
+                                                   bounds_error=False,
+                                                   fill_value=np.nan)
+pred_log_ruwe = np.concatenate([f_ruwe(ref_inputs_star[i]) 
+                                for i in range(nref)])
+
+print(np.nanmax(pred_log_ruwe),np.nanmin(pred_log_ruwe))
+
+print(np.shape(pred_log_ruwe))
+print(np.shape(pred_log_ruwe2d))
+check_match = np.isclose(pred_log_ruwe,pred_log_ruwe2d,atol=1e-7,equal_nan=True)
+print(len(np.where(check_match)[0]))
+
+for i in np.where(~check_match)[0]:
+    print(ref_inputs_star[i],pred_log_ruwe2d[i],pred_log_ruwe[i])
