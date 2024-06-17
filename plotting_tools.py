d56712b74b0fc6cb370f5a949c8d854dcd484ecd
Stephanie Douglas <stephanietdouglas@gmail.com>
2024-06-17 17:30:12 -0400

    update plots to use the .h5 files if available

--- plotting_tools.py
+++ plotting_tools.py
@@ -13,6 +13,7 @@ import scipy.optimize
 from scipy.ndimage.filters import gaussian_filter
 from matplotlib import rcParams
 import yaml
+import h5py
 
 rcParams['font.family'] = 'serif'
 rcParams['font.serif'] = ['Georgia']
@@ -45,12 +46,17 @@ def a_to_period(a):
     return np.sqrt((4*np.pi**2 * a**3) / (2*G)) * 365
 
 def get_info(star):
-    output_path1 = os.path.join(output_dir, "tables/")
-    output_path2 = os.path.join(output_dir, "graphs/")
-    output_path3 = os.path.join(output_dir, "yml/")
+    output_path1 = output_dir #os.path.join(output_dir, "tables/")
+    output_path2 = output_dir #os.path.join(output_dir, "graphs/")
+    output_path3 = output_dir #os.path.join(output_dir, "yml/")
 
-    survivors_file = os.path.expanduser(f'{output_path1}/{star}_kept.csv')
-    all_file = os.path.expanduser(f'{output_path1}/{star}_all.csv')
+    h5file = f'{output_path1}/{star}.h5'
+    if os.path.exists(h5file):
+        all_file = h5file
+        survivors_file = h5file
+    else:
+        survivors_file = os.path.expanduser(f'{output_path1}/{star}_kept.csv')
+        all_file = os.path.expanduser(f'{output_path1}/{star}_all.csv')
     yaml_file = os.path.expanduser(f'{output_path3}/{star}_params_output.yml')
 
     out_file1 = os.path.expanduser(f'{output_path2}/{star}_output_corner.pdf')
@@ -95,16 +101,24 @@ def corner_plot(star, file_out=True, given_params='auto', smoothing=False, color
     fs = 18  # fontsize
     rcParams['xtick.labelsize'] = 'large'
     rcParams['ytick.labelsize'] = 'large'
-    file_in = get_info(star)[0]
+    file_in = get_info(star)[0] #survivors
     n_gen = get_info(star)[6]
     
-    # Arrange Data
-    t = Table.read(file_in, format='ascii.csv')
-    
+    # Read in data
+    if file_in.endswith(".csv"):
+        t = Table.read(survivors_file, comment='#', format='ascii.csv')
+    elif file_in.endswith(".h5") or file_in.endswith(".hdf5"):
+        tfile = h5py.File(file_in)
+        t = tfile["kept"]
+    else:
+        print("file type unknown:",file_in)
+        return
+
+
     P = t['period(days)']
     mass_ratio = t['mass ratio']
     e = t['eccentricity']
-    cos_i = abs(t['cos_i'])
+    cos_i = np.abs(t['cos_i'])
     a  = t['semi-major axis(AU)']
     # Get data ranges
     P_range = [min(P), max(P)]
@@ -354,7 +368,12 @@ def corner_plot(star, file_out=True, given_params='auto', smoothing=False, color
     if file_out is not None:
         out_file = get_info(star)[2]
         plt.savefig(out_file, bbox_inches='tight', pad_inches=0.25, dpi=300)
-    # plt.show()
+        if out_file.endswith(".pdf"):
+            plt.savefig(out_file.replace(".pdf",".png"), 
+                        bbox_inches='tight', pad_inches=0.2, dpi=300)
+
+    if file_in.endswith(".h5") or file_in.endswith(".hdf5"):
+        tfile.close()
     
     return 0
 
@@ -372,16 +391,30 @@ def detection_limits(star, file_out=True, mark_P=None):
     rcParams['xtick.labelsize'] = 'small'
     rcParams['ytick.labelsize'] = 'small'
     rcParams['axes.labelsize'] = 10
-    file_in = get_info(star)[0]
+    file_in = get_info(star)[0] #survivors
     star_mass = get_info(star)[5]
     # print(f"Star mass: {star_mass}")
 
     fig, ax = plt.subplots(figsize=(3.352242, 2.514181))  # this is sized to fit in one column of AAS journal format
     # Read in survivor data
-    survivors = Table.read(file_in, format='ascii.csv')
+    if file_in.endswith(".csv"):
+        survivors = Table.read(file_in, format='ascii.csv')
+    elif file_in.endswith(".h5") or file_in.endswith(".hdf5"):
+        t = h5py.File(file_in,'r+')
+        survivors = t["kept"]
+    else:
+        print("file type unknown:",file_in)
+        return
     # Need to split into logarithmic period bins and take the 95th percentile of the masses in each bin
     # print(survivors['mass ratio'])
-    survivors['mass (M_Jup)'] = survivors['mass ratio']*star_mass*1047.35
+    try:
+        survivors['mass (M_Jup)'] = survivors['mass ratio'][:]*star_mass*1047.35
+    except:
+        print("mass (M_Jup) array already exists!")
+    try:
+        survivors['logPeriod'] = np.log10(survivors['period(days)'])
+    except:
+        print("logPeriod array already exists!")
 
     # Bin
     bins = np.arange(0., 10., .5)
@@ -393,14 +426,21 @@ def detection_limits(star, file_out=True, mark_P=None):
     sixty = []
     n = []
     for j in range(1, len(bins)):
-        survivor_bin = survivors[[bins[j-1] <= np.log10(x) < bins[j] for x in survivors['period(days)']]]
-        if len(survivor_bin) > 0:
-            ninety_five.append(np.percentile(survivor_bin['mass (M_Jup)'], 95))
-            ninety.append(np.percentile(survivor_bin['mass (M_Jup)'], 90))
-            sixty.append(np.percentile(survivor_bin['mass (M_Jup)'], 60))
-            period.append(np.median(survivor_bin['period(days)']))
-            n.append(len(survivor_bin))
-    # Plot
+        in_bin = (bins[j-1] <= survivors['logPeriod']) & (survivors['logPeriod'] <= bins[j])
+        # survivor_bin = survivors[[bins[j-1] <= np.log10(x) < bins[j] for x in survivors['period(days)']]]
+        # if len(survivor_bin) > 0:
+        #     ninety_five.append(np.percentile(survivor_bin['mass (M_Jup)'], 95))
+        #     ninety.append(np.percentile(survivor_bin['mass (M_Jup)'], 90))
+        #     sixty.append(np.percentile(survivor_bin['mass (M_Jup)'], 60))
+        #     period.append(np.median(survivor_bin['period(days)']))
+        #     n.append(len(survivor_bin))
+        nbin = len(np.where(in_bin)[0])
+        if nbin > 0:
+            ninety_five.append(np.percentile(survivors['mass (M_Jup)'][in_bin], 95))
+            ninety.append(np.percentile(survivors['mass (M_Jup)'][in_bin], 90))
+            sixty.append(np.percentile(survivors['mass (M_Jup)'][in_bin], 60))
+            period.append(np.median(survivors['period(days)'][in_bin]))
+            n.append(nbin)    # Plot
     ax.plot(period, ninety_five, marker='o', ms=4, color='#4d9de0', label='95th percentile')
     ax.plot(period, ninety, marker='o', ms=4,  color='#e15554', label='90th')
     ax.plot(period, sixty, marker='o', ms=4, color='#836c9f', label='60th')
@@ -430,121 +470,106 @@ def detection_limits(star, file_out=True, mark_P=None):
     if file_out:
         out_file = get_info(star)[3]
         plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2, dpi=300)
+        if out_file.endswith(".pdf"):
+            plt.savefig(out_file.replace(".pdf",".png"), 
+                        bbox_inches='tight', pad_inches=0.2, dpi=300)
 
     # plt.show()
+    if file_in.endswith(".h5") or file_in.endswith(".hdf5"):
+        t.close()
 
     return
 
 def survivor_plot(star, param, file_out=False):
-  # Creates three panel plot with histograms showing generated, and surviving parameter distribution and bar plot showing survivor fraction
-  # Inputs:
-  #     survivors_file - filename for kept companions. This is the _kept.csv generated by  MOLUSC
-  #     all_file - filename for all companions. This is the _all.csv generated by MOLUSC
-  #     param  - orbital parameter to make the plot for. Can be either period (P)  or mass ratio (m)
-  #     file_out - optional, filename to write image to when complete
-  # Load kept and survivors
-  survivors_file = get_info(star)[0]
-  all_file = get_info(star)[1]
-  # print(f'Survivors file: {survivors_file}\n All file: {all_file}\n')
+    # Creates three panel plot with histograms showing generated, and surviving parameter distribution and bar plot showing survivor fraction
+    # Inputs:
+    #     survivors_file - filename for kept companions. This is the _kept.csv generated by  MOLUSC
+    #     all_file - filename for all companions. This is the _all.csv generated by MOLUSC
+    #     param  - orbital parameter to make the plot for. Can be either period (P)  or mass ratio (m)
+    #     file_out - optional, filename to write image to when complete
+    # Load kept and survivors
+    survivors_file = get_info(star)[0]
+    all_file = get_info(star)[1]
+    # print(f'Survivors file: {survivors_file}\n All file: {all_file}\n')
 
-  t = Table.read(survivors_file, comment='#', format='ascii.csv')
-  t_all = Table.read(all_file, comment='#', format='ascii.csv')
-  # Figure Options
-  rcParams['xtick.labelsize'] = 'small'
-  rcParams['ytick.labelsize'] = 'small'
-  rcParams['axes.labelsize'] = 10
-  rcParams['hatch.linewidth'] = 1.5
+    if all_file.endswith(".csv") and survivors_file.endswith(".csv"):
+        t = Table.read(survivors_file, comment='#', format='ascii.csv')
+        t_all = Table.read(all_file, comment='#', format='ascii.csv')
+    elif all_file.endswith(".h5") or all_file.endswith(".hdf5"):
+        tfile = h5py.File(all_file)
+        t = tfile["kept"]
+        t_all = tfile["all"]
+    else:
+        print("file type unknown:",all_file,survivors_file)
+        return
+    # Figure Options
+    rcParams['xtick.labelsize'] = 'small'
+    rcParams['ytick.labelsize'] = 'small'
+    rcParams['axes.labelsize'] = 10
+    rcParams['hatch.linewidth'] = 1.5
 
-  fig, axes = plt.subplots(3, 1, sharex='col', figsize=(3.352242, 3.352242)) #  sized for one column of AAS journal format
+    fig, axes = plt.subplots(3, 1, sharex='col', figsize=(3.352242, 3.352242)) #  sized for one column of AAS journal format
 
-  if param == 'P' or param == 'period':
-      # Plot two histograms, top one a histogram of period (with log bins), bottom one a plot of survivorship fraction
-      bins = np.logspace(0, 10, 20)
-      n, _ = np.histogram(t['period(days)'], bins=bins)
-      N, _ = np.histogram(t_all['period(days)'], bins=bins)
-      scale = 'log'
-      axes[2].set_xlabel('Period (days)')
-  elif param == 'm' or param == 'mass ratio' or param == 'mass_ratio':
-      # Plot two histograms, top one a histogram of mass, bottom one a plot of survivorship fraction
-      bins = np.linspace(0, 1, 20)
-      n, _ = np.histogram(t['mass ratio'], bins=bins)
-      N, _ = np.histogram(t_all['mass ratio'], bins=bins)
-      scale = 'linear'
-      axes[2].set_xlabel('Mass Ratio')
-
-    
-  widths = [bins[i + 1] - bins[i] for i in range(len(bins) - 1)]
-
-  # Top plot, generated histogram
-  steps = np.concatenate((N/sum(N), [(N/sum(N))[-1]]))  # steps will stop at the start of the last bar unless repeated
-  axes[0].step(bins, steps, where='post', c='k', linewidth=2)
-  axes[0].bar(bins[:-1], N / sum(N), widths, align='edge', linewidth=1, color='#4d9de0', edgecolor='#4d9de0')
-  # middle plot, surviving histogram
-  steps = np.concatenate((n/sum(N), [(n/sum(N))[-1]]))
-  axes[1].step(bins, steps, where='post', c='k', linewidth=2)
-  axes[1].bar(bins[:-1], n / sum(N), widths, align='edge', linewidth=1, color='#4d9de0', edgecolor='#4d9de0')
-  # Bottom plot, surviving fraction
-  steps = np.concatenate((n/N, [(n/N)[-1]]))
-  axes[2].step(bins, steps, where='post', c='k', linewidth=2)
-  axes[2].bar(bins[:-1], n / N, widths, align='edge', linewidth=1, color='#4d9de0', edgecolor='#4d9de0')
-
-   # Set axis labels and limits
-  axes[0].set_ylabel('Generated\nFrequency')
-  axes[1].set_ylabel('Survivor\nFrequency')
-  axes[2].set_ylabel('Surviving\nFraction')
-  axes[0].set_xlim(bins[0], bins[-1])
-  axes[1].set_xlim(bins[0], bins[-1])
-  axes[2].set_xlim(bins[0], bins[-1])
-
-  # Add a secondary x axis, showing semi-major axis for a solar mass companion
-  secax = axes[0].secondary_xaxis('top', functions=(period_to_a, a_to_period))
-  secax.set_xlabel('a (AU)')
-  secax.tick_params(axis='x', which='minor', bottom=False, top=False)
-
-  # Set to log scale if plotting period, or linear for mass ratio
-  for a in axes:
-      a.set_xscale(scale)
-
-  if file_out is not None:
-      out_file = get_info(star)[4]
-      # print(f'Out file: {out_file}')
-      plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2, dpi=300)
-  # plt.show()
-  return
-
-### I'm saving my old main version for reference
-# if __name__=="__main__":
-
-#     # yaml_fname = "code/yml_test2_params_output.yml"
-#     yaml_fname = "code/prae_test_params_output.yml"
-#     with open(yaml_fname,"r") as f:
-#         params = yaml.safe_load(f)
-
-#     # Run info
-#     file_dir = "code/"
-#     file_base = params["file_prefix"] #yaml_fname.split("_")[0]
-#     n = params["num_generated"]  # number of companions generated in run
-#     mass = params["Star"]["Mass"]  # target mass in solar masses
+    if param == 'P' or param == 'period':
+        # Plot two histograms, top one a histogram of period (with log bins), bottom one a plot of survivorship fraction
+        bins = np.logspace(0, 10, 20)
+        n, _ = np.histogram(t['period(days)'], bins=bins)
+        N, _ = np.histogram(t_all['period(days)'], bins=bins)
+        scale = 'log'
+        axes[2].set_xlabel('Period (days)')
+    elif param == 'm' or param == 'mass ratio' or param == 'mass_ratio':
+        # Plot two histograms, top one a histogram of mass, bottom one a plot of survivorship fraction
+        bins = np.linspace(0, 1, 20)
+        n, _ = np.histogram(t['mass ratio'], bins=bins)
+        N, _ = np.histogram(t_all['mass ratio'], bins=bins)
+        scale = 'linear'
+        axes[2].set_xlabel('Mass Ratio')
 
 
-#     # Input files
-#     survivors_file = f'{file_dir}{file_base}_kept.csv'
-#     all_file = f'{file_dir}{file_base}_all.csv'
-#     # Output files
-#     out_file = f'{file_base}_corner.pdf'  # writeout file for the corner plots
-#     out_file2 = f'{file_base}_dtct_lims.pdf' # writeout file for the detection limit plots
-#     out_file3 = f'{file_base}_srv.pdf' # writeout file for the survivor plots
+    widths = [bins[i + 1] - bins[i] for i in range(len(bins) - 1)]
 
-#     if os.path.exists(survivors_file):
-#         corner(survivors_file,  n_gen=n, given_params='all', smoothing=True, file_out=out_file)
-#         detection_limits(survivors_file, mass, file_out=out_file2)
-#         if os.path.exists(all_file):
-#             survivor(survivors_file, all_file, param='P', file_out=out_file3)
-#         else:
-#             print(f"\t{all_file} not found; no survivor plot produced")
-#     else:
-#         print(f"\t{survivors_file} not found; no plots produced")
+    # Top plot, generated histogram
+    steps = np.concatenate((N/sum(N), [(N/sum(N))[-1]]))  # steps will stop at the start of the last bar unless repeated
+    axes[0].step(bins, steps, where='post', c='k', linewidth=2)
+    axes[0].bar(bins[:-1], N / sum(N), widths, align='edge', linewidth=1, color='#4d9de0', edgecolor='#4d9de0')
+    # middle plot, surviving histogram
+    steps = np.concatenate((n/sum(N), [(n/sum(N))[-1]]))
+    axes[1].step(bins, steps, where='post', c='k', linewidth=2)
+    axes[1].bar(bins[:-1], n / sum(N), widths, align='edge', linewidth=1, color='#4d9de0', edgecolor='#4d9de0')
+    # Bottom plot, surviving fraction
+    steps = np.concatenate((n/N, [(n/N)[-1]]))
+    axes[2].step(bins, steps, where='post', c='k', linewidth=2)
+    axes[2].bar(bins[:-1], n / N, widths, align='edge', linewidth=1, color='#4d9de0', edgecolor='#4d9de0')
 
+    # Set axis labels and limits
+    axes[0].set_ylabel('Generated\nFrequency')
+    axes[1].set_ylabel('Survivor\nFrequency')
+    axes[2].set_ylabel('Surviving\nFraction')
+    axes[0].set_xlim(bins[0], bins[-1])
+    axes[1].set_xlim(bins[0], bins[-1])
+    axes[2].set_xlim(bins[0], bins[-1])
+
+    # Add a secondary x axis, showing semi-major axis for a solar mass companion
+    secax = axes[0].secondary_xaxis('top', functions=(period_to_a, a_to_period))
+    secax.set_xlabel('a (AU)')
+    secax.tick_params(axis='x', which='minor', bottom=False, top=False)
+
+    # Set to log scale if plotting period, or linear for mass ratio
+    for a in axes:
+        a.set_xscale(scale)
+
+    if file_out is not None:
+        out_file = get_info(star)[4]
+        # print(f'Out file: {out_file}')
+        plt.savefig(out_file, bbox_inches='tight', pad_inches=0.2, dpi=300)
+        if out_file.endswith(".pdf"):
+            plt.savefig(out_file.replace(".pdf",".png"), 
+                        bbox_inches='tight', pad_inches=0.2, dpi=300)
+
+    if all_file.endswith(".h5") or all_file.endswith(".hdf5"):
+        tfile.close()
+
+    return
 
 def plotter(star, corner=True, detlims=True, survivor=True):
     if corner:
@@ -556,18 +581,36 @@ def plotter(star, corner=True, detlims=True, survivor=True):
     
 
 if __name__ == '__main__':
+
+    # Close any open HDF5 files, just in case
+    import tables
+    tables.file._open_files.close_all()
     
     # Theory crafting the setup:
         # Pass a list of stars, return survivor file, all file, out files 1-3
         # If specified, get graphs 1-3 for star(s)
         # Make sure to include n and mass
-        
-    star = "JS355"
-    info = get_info(star)
-    # print(info)
 
-    plotter(star, corner=True, detlims=True, survivor=False)
-    
+    # files = glob.glob(output_dir+"*.yml")
+    # print(files)
+    # stars_raw = [fname.split("/")[-1][:-18] for fname in files]
+    # stars = np.unique(stars_raw)
+        
+    # # star = "JS355"
+    for star in stars:
+        print(star)
+        # info = get_info(star)
+        # print(info)
+
+        plotter(star, corner=True, detlims=True, survivor=False)
+        plt.close("all")
+
+
+    # star = "AD_0738"
+    # info = get_info(star)
+    # plotter(star, corner=True, detlims=True, survivor=True)
+
+
     # corner(survivors_file,  n_gen=n, given_params='all', smoothing=True, file_out=out_file1)
     # detection_limits(survivors_file, mass, file_out=out_file2)
     # survivor(survivors_file, all_file, param='P', file_out=out_file3)
